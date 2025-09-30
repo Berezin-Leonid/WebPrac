d53ad65e5a2ba665a655c6bccc495db91db9ae2a
Berezin-Leonid <Berezinlion@gmail.com>
2025-09-30 09:26:48 +0300

    Done with one table

--- src/main/java/com/berezin/WebPrak/controllers/EmployeeController.java
+++ src/main/java/com/berezin/WebPrak/controllers/EmployeeController.java
@@ -4,13 +4,13 @@ package com.berezin.WebPrak.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 import com.berezin.WebPrak.models.Employee;
 import com.berezin.WebPrak.DAO.EmployeeDAO;
 import com.berezin.WebPrak.DAO.impl.EmployeeDAOImpl;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -41,5 +41,32 @@ public class EmployeeController {
         model.addAttribute("employeeService", employeeDAO);
         return "employee";
     }
+
+    @GetMapping("/employee/add")
+    public String addEmployeePage(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employee_form";
+    }
+
+    @PostMapping("/employees/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeDAO.save(employee);
+        return "redirect:/employees";
+    }
+
+    @PostMapping("/employee/delete")
+    public String deleteEmployee(@RequestParam("employeeId") Integer employeeId, RedirectAttributes redirectAttributes) {
+        Employee employee = employeeDAO.getById(employeeId);
+
+        if (employee == null) {
+            redirectAttributes.addFlashAttribute("error_msg", "Сотрудник с ID " + employeeId + " не найден");
+            return "redirect:/employees";
+        }
+
+        employeeDAO.delete(employee);
+        redirectAttributes.addFlashAttribute("success_msg", "Сотрудник успешно удалён");
+        return "redirect:/employees";
+    }
+
 
 }
